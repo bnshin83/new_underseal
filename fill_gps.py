44b544b1258579b91a32a8b4c9c3fc6b36a4e1b5
Wen Tang <tangwentlw@gmail.com>
2023-05-06 17:37:28 -0400

    Add separate 'fill_gps.py' to add missing GPS to DB

---
+++ fill_gps.py
@@ -0,0 +1,111 @@
+import copy
+import cx_Oracle
+import numpy as np
+import os, traceback
+import pandas as pd
+import db
+
+def put_gps(con, csv_path):
+    """
+    Read GPS data from csv file and fill the GPS columns
+    """
+
+    gps_df = pd.read_csv(csv_path)
+    # convert dmi to feet
+    gps_df["DMI"] = (round(3.28084*gps_df["DMI"])).astype(int)
+
+    # Get the basename of csv path, this base name should match the F25 file name (exclude extension name)
+    csv_basename = os.path.basename(csv_path)[:-8] # Assume it ends with " gps.csv" (notice that there is a white space)
+
+    cursor = con.cursor()
+
+    # Get the longlist id first 
+    idstr = """
+        SELECT LONGLIST_ID FROM stda_longlist
+        WHERE F25_INFO='""" + csv_basename + """'
+        """
+    cursor.execute(idstr)
+    id = None
+    for result in cursor:
+        id = result[0]
+    
+    if id==None:
+        with open(gps_upload_error_log,"a+") as f:
+            print("Cannot find a matching longlist ID for the following input CSV file:",file=f)
+            print(csv_path,file=f)
+            print("Check if the csv name matches the F25 name")
+    # Update begin latitude, begin longitude, end latitude, end longitude
+    begin_gpsx,begin_gpsy = gps_df.sort_values(by=['DMI'],ascending=True).head(1)[["Latitude","Longitude"]].values[0]
+    end_gpsx,end_gpsy = gps_df.sort_values(by=['DMI'],ascending=False).head(1)[["Latitude","Longitude"]].values[0]
+    print("begin_gpsx,begin_gpsy",begin_gpsx,begin_gpsy)
+    cursor.executemany("""
+                    UPDATE stda_longlist
+                    SET begin_latitude = :begin_lat,
+                        begin_longitude = :begin_lon,
+                        end_latitude = :end_lat,
+                        end_longitude = :end_lon
+                    WHERE longlist_id = :id""", 
+                    [{"begin_lat": begin_gpsx, "begin_lon": begin_gpsy, "end_lat": end_gpsx, "end_lon": end_gpsy, "id": id}])
+
+
+    # Update gpsx and gpsy table in stda.calculations
+    # Concatenate id column to array. After concat: column order [feet, latitude, longtitude, id]
+    sql_arr = np.concatenate((np.array(gps_df), np.array([[id]]*gps_df.shape[0])), axis=1)
+    # need to change column order to [latitude, longtitude, feet, id]
+    sql_arr = sql_arr[:,[1,2,0,3]]
+    sql_arr = list(map(tuple,sql_arr))
+
+    query = """
+            UPDATE stda_deflections
+            SET gpsx = :gpsx_val,
+                gpsy = :gpsy_val
+            WHERE longlist_id = :longlist_id_val AND chainage_ft = :chainage_ft_val
+            """
+
+    cursor.executemany(query, [{"gpsx_val": row[0], 
+                                "gpsy_val": row[1], 
+                                "longlist_id_val": id, 
+                                "chainage_ft_val": row[2]} 
+                                for row in sql_arr])
+    
+    con.commit()
+    cursor.close()
+
+
+if __name__ == "__main__":
+    
+    import argparse
+    from tkinter import filedialog
+
+    parser = argparse.ArgumentParser(description='Upload misssing GPS data in batch mode')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--dev_env', action='store_true')
+    args = parser.parse_args()
+
+    batch_csv_txt = filedialog.askopenfilename(initialdir='./',title='Select An External .txt File', 
+                                          filetypes=(("TXT files","*.txt"),("all files","*.*"))
+                                         )
+
+    gps_upload_error_log = os.path.join(os.path.dirname(batch_csv_txt),"GPS_upload_error_log.txt")
+    if os.path.exists(gps_upload_error_log):
+        os.remove(gps_upload_error_log)
+
+    con = db.connect(args.dev_env)
+
+    with open(batch_csv_txt,'r') as file:
+        Lines = file.readlines()
+
+    for line in Lines:
+        csv_path = line.strip('\"')
+        print("csv_path",csv_path)
+        try:
+            put_gps(con, csv_path)
+            print("Successfully upload {}".format(os.path.basename(csv_path)))
+        except:
+            traceback_str = traceback.format_exc()
+            print(traceback_str)
+            with open(gps_upload_error_log,"a+") as f:
+                print("######### Start of error log #########",file=f)
+                print('Input CSV path: {}\n'.format(csv_path),file=f)
+                print(traceback_str,file=f)
+                print("######### End of error log #########\n\n",file=f)
